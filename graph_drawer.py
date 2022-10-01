c4b5cfbc4cc390c19ba1a3269e9ceec8fb77b9b5
Ilya Ufimtsev <91266234+Ilya-U@users.noreply.github.com>
2022-10-01 11:40:07 +0500

    graph_drawer изменён.
    Выделил класс Line.

--- graph_drawer.py
+++ graph_drawer.py
@@ -2,34 +2,36 @@ import matplotlib.pyplot as plt
 
 
 class GraphDrawer:
-    def __init__(self, y_cos_limit, y_sin_limit) -> None:
-        # TODO Необходимо выделить класс Line, чтобы избежать дублирования кода!!!
+    def __init__(self, y_cos_limit: float, y_sin_limit: float, cos_label: str, sin_label: str) -> None:
         plt.ion()
         self.fig = plt.figure()
 
         self.y_sin_limit = y_sin_limit
         self.y_cos_limit = y_cos_limit
 
-        self.time_limit = 30
+        self.time_limit = 15
 
-        self.cos_line = Line(self.fig.add_subplot(211), self.time_limit, self.y_cos_limit, "red")
-        self.sin_line = Line(self.fig.add_subplot(212), self.time_limit, self.y_sin_limit, "blue")
+        self.cos_line = Line(self.fig.add_subplot(211), self.time_limit, self.y_cos_limit, "red", cos_label)
+        self.sin_line = Line(self.fig.add_subplot(212), self.time_limit, self.y_sin_limit, "blue", sin_label)
 
     def update(self, time, new_cos_data, new_sin_data):
-        self.cos_line.add_data(time, new_cos_data)
-        self.sin_line.add_data(time, new_sin_data)
+        if time <= self.time_limit:
+            self.cos_line.add_data(time, new_cos_data)
+            self.sin_line.add_data(time, new_sin_data)
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
 
 class Line:
-    def __init__(self, ax, time_limit, y_limit, color) -> None:
+    def __init__(self, ax, time_limit: float, y_limit: float, color: str, ylabel: str, xlabel: str="Время") -> None:
         self.ax = ax
         self.time_limit = time_limit
         self.y_limit = y_limit
 
         self.ax.set_xlim(0, self.time_limit)
         self.ax.set_ylim(-self.y_limit, self.y_limit)
+        self.ax.set_ylabel(ylabel)
+        self.ax.set_xlabel(xlabel)
         self.data_x = []
         self.data_y = []
         self.line, = self.ax.plot(self.data_x, self.data_y, color=color)
